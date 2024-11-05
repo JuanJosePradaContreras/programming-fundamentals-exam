47f26530c50e545f09d39c62af9c2f3e5a35b34d
JuanJosePradaContreras <180313522+JuanJosePradaContreras@users.noreply.github.com>
2024-11-05 12:49:11 -0500

    Solution excercise 4

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-#Ejercicio 3
+#Ejercicio 4
 
-volt_1 = float(input("ingrese el primer voltaje: "))
-volt_2 = float(input("ingrese el segundo voltaje: "))
-volt_3 = float(input("ingrese el tercero voltaje: "))
+km = float(input("Ingrese la medida: "))
 
-result = (volt_1 + volt_2 + volt_3) / 3
+result = km / 1000
 
-if result < 115:
-    print(f"""VOLTAJE CORRECTO""")
-
-if result > 115 and result < 220:
-    print(f"""ALTO VOLTAJE""")
-
-if result > 220:
-    print(f"""PELIGRO""")
+print(f"""{km} m, son {result} km""")
